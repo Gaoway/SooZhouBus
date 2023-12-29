2eb87406dd9353a714bc9c92d0ac4f275fbb43f2
gw <2085438633@qq.com>
2023-12-29 19:24:46 +0800

    v2.1

--- msgbx.py
+++ msgbx.py
@@ -1,10 +1,43 @@
 from tkinter import messagebox
 import tkinter.messagebox
 from tkinter import *
+import ctypes
+import ctypes.wintypes
+from win10toast import ToastNotifier
 
 #a = messagebox.showinfo("提示","我是一个提示框")
 #messagebox.showwarning(title='警告', message='这是一条警告') 
-root=Tk()
+#root=Tk()
 
-root.withdraw()#隐藏主窗口
-tkinter.messagebox.showinfo(title='提示',message='设置成功')
+#root.withdraw()#隐藏主窗口
+#a = tkinter.messagebox.showinfo(title='提示',message='设置成功')
+
+# 定义 MessageBoxW 函数
+#MessageBox = ctypes.windll.user32.MessageBoxW
+#MessageBoxW = ctypes.windll.user32.MessageBoxW
+#
+## 定义 MessageBox 参数
+#MB_OK = 0
+#MB_OKCANCEL = 1
+#MB_YESNOCANCEL = 3
+#MB_YESNO = 4
+#
+## 显示系统提示框
+## 参数：窗口句柄、文本、标题、类型
+#result = MessageBox(None, '这是一条提示信息', '提示', MB_OK)
+#if result == 1:
+#    print("用户点击了OK")
+#elif result == 2:
+#    print("用户点击了Cancel")
+
+toaster = ToastNotifier()
+# 显示通知
+# 参数：标题、消息、图标（可选）、持续时间
+toaster.show_toast("通知标题", "这是通知内容", duration=1)
+
+# 注意：程序需要保持运行状态直到通知结束
+import time
+while toaster.notification_active(): time.sleep(0.1)
+time.sleep(5)
+print('done')
+while toaster.notification_active(): time.sleep(0.1)
--- std.py
+++ std.py
@@ -12,15 +12,15 @@ class szgj:
         self.len = 4
         self.name = '1021'
 
-    def get_bus_info(self):
-        businfo = requests.get(self.url)
+    def get_bus_info(self, url):
+        businfo = requests.get(url)
         businfo_json    = json.loads(businfo.text)
         self.businfo    = businfo_json['data']['standInfo']
 
         return self.businfo
        
     def load_info(self):
-        self.get_bus_info()
+        self.get_bus_info(self.url)
         self.bus_serial = []
         for i in self.businfo:
             if len(self.businfo[i]) == 0:
@@ -41,7 +41,7 @@ class szgj:
                 if self.ustc_gap > gap:
                     self.ustc_gap = gap
         print(self.name,'bus to ustc: ', gap_list)
-        return self.ustc_gap
+        return gap_list
 
     def sushe_req(self):
         self.load_info()
@@ -68,6 +68,7 @@ class szgj:
 
 class bus1021(szgj):
     def __init__(self) -> None:
+        super().__init__()
         self.url = 'https://szgj.2500.tv/api/v1/busline/bus?line_guid=0000000000LINELINEINFO18082357212871'
         self.suse = 12
         self.ustc = 24
@@ -76,16 +77,54 @@ class bus1021(szgj):
 
 class bus143(szgj):
     def __init__(self) -> None:
+        super().__init__()
         self.url = 'https://szgj.2500.tv/api/v1/busline/bus?line_guid=1aa773c8-e865-4847-95a1-f4c956ae02ef'
         self.suse = 0
         self.ustc = 29
         self.len = 3
         self.name = '143'
 
-if __name__ == '__main__':
-    szgj    = bus143()
-    szgj.monitor_ustc()
-    
+class bus146(szgj):
+    def __init__(self) -> None:
+        super().__init__()
+        self.url = 'https://szgj.2500.tv/api/v1/busline/bus?line_guid=96c177f0-0828-4a31-ba19-ddce9e9649e0'
+        self.suse = 0
+        self.ustc = 22
+        self.len = 3
+        self.name = '146'
 
-        
-        
+class busall(bus1021, bus143, bus146):
+    def __init__(self) -> None:
+        super().__init__()
+        self.bus1021 = bus1021()
+        self.bus143 = bus143()
+        self.bus146 = bus146()
+
+    def monitor_ustc(self):
+        i=40
+        buslist = newlist =  [self.bus1021, self.bus143, self.bus146]
+        while(i>=0):
+            i=i-1
+            buslist = newlist
+            newlist = []
+            for bus in buslist:
+                gap_list = bus.ustc_req()       
+                if len(gap_list) != 0:
+                    newlist.append(bus)
+                if (bus.ustc_gap == bus.len or bus.ustc_gap == bus.len-1):
+                    print(f'{bus.name} 即将进站 {bus.ustc_gap}')
+                    if i >= 10:
+                        i = 10
+                time.sleep(1)
+            time.sleep(30)
+
+
+if __name__ == '__main__':
+    #szgj146    = bus146()
+    #szgj1021   = bus1021()
+    #szgj143    = bus143()
+    #szgj1021.monitor_ustc()
+    #szgj146.monitor_ustc()
+    #szgj146.monitor_ustc()
+    szgjall = busall()
+    szgjall.monitor_ustc()
